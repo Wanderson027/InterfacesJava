509e84dc0068b8bf3bc621ecf6fe093a73c8f344
Wanderson Rafael <w.rafael.s@hotmail.com>
2019-06-11 12:26:50 -0300

    Rafael

--- src/CadastrarClientes.java
+++ src/CadastrarClientes.java
@@ -30,7 +30,7 @@ public class CadastrarClientes extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Voltar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -42,7 +42,7 @@ public class CadastrarClientes extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(628, Short.MAX_VALUE)
+                .addContainerGap(640, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addContainerGap())
         );
